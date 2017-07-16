c4c738c1ef11d162561b582b708d4266e10f61d5
Tomasz Pawłowski <t.pawlowski@live.com>
2017-07-16 19:59:54 +0200

    add json-server and testing connection

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
     console.log("hello jQuery");
+    var localJsonServerURL = "http://localhost:3000/db";
+
+    function loadAPI(){
+        $.ajax({
+        url: localJsonServerURL, 
+        dataType: "json"
+        }).done(function(response){
+        console.log(response);
+        }).fail(function(error){
+        console.log(error);
+        })
+  }
+  loadAPI();
+
 });
